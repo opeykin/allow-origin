e120fb369b218bed50d7d506d4d046e5476bbb8a
alexander.opeykin <alexander.opeykin@gmail.com>
2017-07-31 15:59:19 +0200

    return JSON with timestamp status and date representation from timestamp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,3 +10,12 @@ def hello():
     return "Hello World!11111"
 
 
+@app.route("/<datestr>")
+def timestamp(datestr):
+    try:
+        times = int(datestr)
+        return jsonify({"status": "OK",
+                        "date": datetime.utcfromtimestamp(times).strftime('%Y-%m-%dT%H:%M:%SZ'),
+                        "timestamp": times})
+    except ValueError:
+        return jsonify({"status": "INVALID_REQUEST"})
