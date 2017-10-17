a700cbaf59818d85187587d6c0a1b4cfcc96538c
alexander.opeykin <alexander.opeykin@gmail.com>
2017-10-17 23:14:05 +0300

    implement proxy

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-from datetime import datetime
-
-from flask import Flask, jsonify
+from flask import Flask, Response, request
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!11111"
+    return "Usage: url + site api request. Will mirror request with allow origin *"
 
 
-@app.route("/<datestr>")
-def timestamp(datestr):
-    try:
-        times = int(datestr)
-        return jsonify({"status": "OK",
-                        "date": datetime.utcfromtimestamp(times).strftime('%Y-%m-%d %H:%M:%S'),
-                        "timestamp": times})
-    except ValueError:
-        return jsonify({"status": "INVALID_REQUEST"})
+@app.route("/<path:path>")
+def redirect(path):
+    r = requests.get("https://" + path, params=request.args)
+    resp = Response(r.text)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
